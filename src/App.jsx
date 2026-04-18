7bb66b25e0a425b660afc2bebfed4c8dbe825428
Abinex3 <abineshmuniyasaamy@gmail.com>
2026-04-18 21:50:30 +0530

    Hero Section v1 Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
+import HeroSection from "./pages/HeroSection";
+
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Personal Portfolio
-      </h1>
+    <div>
+      <HeroSection />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
